9022edfc4da0d704fc3f2ab6a9c4fde4aefb6f94
basilboli <Agent0295>
2016-08-09 17:36:21 +0200

    refactoring

---
+++ server/websockets/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"bitbucket.org/basilboli/boomer/server/ws"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+
+	// websocket server
+	server := ws.NewServer("/entry")
+	go server.Listen()
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
---
+++ server/websockets/ws/client.go
@@ -0,0 +1,124 @@
+package ws
+
+import (
+	"fmt"
+	"io"
+	"log"
+
+	"golang.org/x/net/websocket"
+)
+
+const channelBufSize = 100
+
+var maxId int = 0
+
+// Chat client.
+type Client struct {
+	id     int
+	ws     *websocket.Conn
+	server *Server
+	ch     chan *Message
+	doneCh chan bool
+	name   string
+	lat    string
+	lng    string
+}
+
+// Create new chat client.
+func NewClient(ws *websocket.Conn, server *Server) *Client {
+
+	if ws == nil {
+		panic("ws cannot be nil")
+	}
+
+	if server == nil {
+		panic("server cannot be nil")
+	}
+
+	maxId++
+	ch := make(chan *Message, channelBufSize)
+	doneCh := make(chan bool)
+
+	return &Client{maxId, ws, server, ch, doneCh, "", "", ""}
+}
+
+func (c *Client) Conn() *websocket.Conn {
+	return c.ws
+}
+
+func (c *Client) Write(msg *Message) {
+	select {
+	case c.ch <- msg:
+	default:
+		c.server.Del(c)
+		err := fmt.Errorf("client %d is disconnected.", c.id)
+		c.server.Err(err)
+	}
+}
+
+func (c *Client) Done() {
+	c.doneCh <- true
+}
+
+// Listen Write and Read request via chanel
+func (c *Client) Listen() {
+	go c.listenWrite()
+	c.listenRead()
+}
+
+// Listen write request via chanel
+func (c *Client) listenWrite() {
+	log.Println("Listening write to client")
+	for {
+		select {
+
+		// send message to the client
+		case msg := <-c.ch:
+			log.Println("Send:", msg)
+			websocket.JSON.Send(c.ws, msg)
+
+		// receive done request
+		case <-c.doneCh:
+			c.server.Del(c)
+			c.doneCh <- true // for listenRead method
+			return
+		}
+	}
+}
+
+// Listen read request via chanel
+func (c *Client) listenRead() {
+	log.Println("Listening read from client")
+	for {
+		select {
+
+		// receive done request
+		case <-c.doneCh:
+			c.server.Del(c)
+			c.doneCh <- true // for listenWrite method
+			return
+
+		// read data from websocket connection
+		default:
+			var msg Message
+			err := websocket.JSON.Receive(c.ws, &msg)
+			log.Printf("Incoming message type is %s\n", msg.Type == LocUpdate)
+			if err == io.EOF {
+				c.doneCh <- true
+			} else if err != nil {
+				c.server.Err(err)
+			} else {
+				if msg.Type == LocUpdate {
+					c.name = msg.Name
+					c.lat = msg.Lat
+					c.lng = msg.Lng
+					c.server.SendLocUpdates(&msg)
+				} else if msg.Type == ChatMessage {
+					c.server.SendAll(&msg)
+				} else {
+					c.server.Err(err)
+				}
+			}
+		}
+	}
+}
