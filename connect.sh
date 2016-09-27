a2d36be0a086440a73cec92b6a39d60148921291
Vasyl Vaskul <basilboli@gmail.com>
2016-09-27 16:26:36 +0200

    connect.sh

--- connect.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS=/Users/basilboli/dev/google/serviceaccount/boomer-92c5f59548fa.json
-gcloud config configurations activate boomer
-gcloud config set project boomer-1470064436690
-gcloud container clusters get-credentials boomer-cluster
